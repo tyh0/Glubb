2f8f6ecf6fd7b1af1a869c2e7cf23b1f30fd5c1c
Tyler House <tyhouse12@gmail.com>
2015-05-23 18:21:08 -0700

    added the xaml.cs and csproj files that had been changed

--- Glubb/GamePage.xaml.cs
+++ Glubb/GamePage.xaml.cs
@@ -22,7 +22,7 @@ namespace Glubb
     /// </summary>
     public sealed partial class GamePage : Page
     {
-		readonly Game1 _game;
+		readonly TheGame _game;
 
 		public GamePage()
         {
@@ -30,7 +30,7 @@ namespace Glubb
 
 			// Create the game.
 			var launchArguments = string.Empty;
-            _game = MonoGame.Framework.XamlGame<Game1>.Create(launchArguments, Window.Current.CoreWindow, swapChainPanel);
+            _game = MonoGame.Framework.XamlGame<TheGame>.Create(launchArguments, Window.Current.CoreWindow, swapChainPanel);
         }
     }
 }
